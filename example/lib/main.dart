3ada121ebecbfc1d3485185599d6b8abe49a603b
meetqy <meetqy@qq.com>
2022-01-20 15:47:29 +0800

    feat: add slate colors

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tailwindcss/core/tw_colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,16 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: TwColor.slate,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -74,6 +66,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        backgroundColor: Colors.red,
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
--- lib/flutter_tailwindcss.dart
+++ lib/flutter_tailwindcss.dart
@@ -1,7 +1,3 @@
 library flutter_tailwindcss;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export './core/tw_colors.dart';
